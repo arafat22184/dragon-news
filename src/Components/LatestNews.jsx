e1677fd6029358b045d0d0ece9d820e8f57182a8
arafat22184 <123alarafat@gmail.com>
2025-05-01 04:54:17 +0600

    added marquee and navbar and login btn

---
+++ src/Components/LatestNews.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const LatestNews = () => {
+  return (
+    <div className="flex items-center  gap-3 p-3 bg-base-200">
+      <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
+      <Marquee className="flex gap-5" pauseOnHover={true} speed={60}>
+        <p className="font-bold">
+          1 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+        </p>
+        <p className="font-bold">
+          2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+        </p>
+        <p className="font-bold">
+          3 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+        </p>
+      </Marquee>
+    </div>
+  );
+};
+
+export default LatestNews;
